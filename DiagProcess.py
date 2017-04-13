a78dddcf2e145f7907a969a2209ea48bc99eea82
Strideradu <dunan00001@hotmail.com>
2017-04-12 20:22:09 -0400

    uodate

--- DiagProcess.py
+++ DiagProcess.py
@@ -191,6 +191,8 @@ class DiagProcess(object):
                     self.rc_I.append((chain[-1][0], len(rc_chain), -1))
                     self.rc_L.insert(chain[-1][1], len(rc_chain))
                     print chain[-1]
+                    print self.rc_L
+                    print self.rc_I
                     rc_chain.append((chain, l))
                     self.rc_seeds += len(chain)
                     # print chain[0]
