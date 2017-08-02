9be11fb5884277cc3069704aa3f3271b115b6e17
Strideradu <dunan00001@hotmail.com>
2017-08-02 13:51:33 -0400

    change parameter

--- chain_yass.py
+++ chain_yass.py
@@ -438,7 +438,7 @@ if __name__ == '__main__':
         group_hit = GroupHit(line)
         # print group_hit.groups
 
-        group_hit.chain_groups(accuracy=0.85, group_distance=L, rechain_threshold=5, span_coefficient=1.0)
+        group_hit.chain_groups(accuracy=0.85, group_distance=L, rechain_threshold=3, span_coefficient=1.0)
         # print group_hit.chain_align
         if group_hit.aligned:
             output_str = group_hit.query + "\t" + group_hit.target + "\t" + str(group_hit.aligned)
