e307d6625a035f5a7261687821710ef6164151d3
Nan Du <Strideradu@users.noreply.github.com>
2017-08-22 09:58:24 -0400

    for multiple file in single plot

--- KmerProfile_fromOutput.py
+++ KmerProfile_fromOutput.py
@@ -10,65 +10,76 @@ def load_obj(filename):
 
 overlap_dict = load_obj("/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170317_ROC/overlap.pkl")
 
-
 true = []
 false = []
-with open("/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/15mer/compare_kmer_00_15mer.out")as result_file:
-    for line in result_file:
 
-        # check first symble of seq id
-        if line[0] == "m":
-            line_sp = line.strip.split()
-            query_id  = line_sp[0]
-            target_id = line_sp[1]
-            num_hits = int(line_sp[2])
+file_list = [
+    "/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/9mer/compare_kmer_all_9mer.out",
+    "/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/11mer/compare_kmer_all_11mer.out",
+    "/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/12mer/compare_kmer_all_12mer.out",
+    "/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/13mer/compare_kmer_all_13mer.out",
+    "/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/14mer/compare_kmer_all_14mer.out",
+    "/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/15mer/compare_kmer_all_15mer.out"
+]
 
-            if query_id != target_id:
-                if overlap_dict.get(query_id, False) and (
-                            target_id in overlap_dict[query_id][0] or target_id in overlap_dict[query_id][
-                        1] or target_id in overlap_dict[query_id][2]):
-                    true.append(num_hits)
-
-                else:
-                    false.append(num_hits)
-
-true.sort()
-false.sort()
-true_count = len(true)
-false_count = len(false)
-max_value = max(true[-1], false[-1])
-
-sen_list = []
-FPR_list = []
-
-j1 = 0
-j2 = 0
-for i in range(max_value):
-    while j1 < true_count:
-        if true[j1] > i:
-            j1 = j1 - 1
-            if j1 < 0:
-                j1 = 0
-            break
-
-        j1 += 1
-
-    true_positive = true_count - j1
-
-    while j2 < false_count:
-        if false[j2] > i:
-            j2 = j2 - 1
-            if j2 < 0:
-                j2 = 0
-            break
-        j2 += 1
-
-    false_positive = false_count - j2
-    sensitivity = float(true_positive) / true_count
-    FPR = float(false_positive) / false_count
-    sen_list.append(sensitivity)
-    FPR_list.append(FPR)
 plt.figure()
-plt.plot(FPR_list, sen_list)
-#plt.xlim(xmin=-0.0005, xmax=0.012)
-plt.savefig("/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/k_15.png")
+for file in file_list:
+    with open(file)as result_file:
+        for line in result_file:
+
+            # check first symble of seq id
+            if line[0] == "m":
+                line_sp = line.strip().split()
+                query_id = line_sp[0]
+                target_id = line_sp[1]
+                num_hits = int(line_sp[2])
+
+                if query_id != target_id:
+                    if overlap_dict.get(query_id, False) and (
+                                        target_id in overlap_dict[query_id][0] or target_id in overlap_dict[query_id][
+                                    1] or target_id in overlap_dict[query_id][2]):
+                        true.append(num_hits)
+
+                    else:
+                        false.append(num_hits)
+
+    true.sort()
+    false.sort()
+    true_count = len(true)
+    false_count = len(false)
+    max_value = max(true[-1], false[-1])
+
+    sen_list = []
+    FPR_list = []
+
+    j1 = 0
+    j2 = 0
+    for i in range(max_value):
+        while j1 < true_count:
+            if true[j1] > i:
+                j1 = j1 - 1
+                if j1 < 0:
+                    j1 = 0
+                break
+
+            j1 += 1
+
+        true_positive = true_count - j1
+
+        while j2 < false_count:
+            if false[j2] > i:
+                j2 = j2 - 1
+                if j2 < 0:
+                    j2 = 0
+                break
+            j2 += 1
+
+        false_positive = false_count - j2
+        sensitivity = float(true_positive) / true_count
+        FPR = float(false_positive) / false_count
+        sen_list.append(sensitivity)
+        FPR_list.append(FPR)
+
+    plt.plot(FPR_list, sen_list, label = file.split("_")[-1])
+plt.xlim(xmin=-0.0005, xmax=0.1005)
+plt.savefig("/mnt/home/dunan/Job/2016/201605_align_noisy_long-reads/20170821_kmer_retest/kmer_compare.png")
